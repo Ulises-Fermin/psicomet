2520ca262509c168106f388598cfac40d4d97529
wiltv <wvera@correo.unimet.edu.ve>
2021-11-19 16:19:03 -0400

    Barra buscar especialista

--- src/Pages/Quest/Quest.jsx
+++ src/Pages/Quest/Quest.jsx
@@ -4,9 +4,17 @@ import styles from "./Quest.module.css";
 
 function Quest() {
     return (
-      <div id={styles.body}>
-          <h1>Buscar especialista</h1>
-      </div>
+        <div id={styles.body}>
+            <h1 id={styles.title}>Buscar especialista</h1>
+            <div id={styles.containerQuest}>
+                <input type="text" placeholder="Introduzca nombre del especialista" id={styles.name}></input>
+                <button id={styles.button}>Buscar</button>
+            </div>    
+            <div id={styles.container}>
+                <p>aqui va aja</p>
+            </div>
+          
+        </div>
     );
   }
   
