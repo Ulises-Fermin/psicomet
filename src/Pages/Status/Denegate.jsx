5e98ca57e0ab02bfcddccd6ce42fd0497347430a
wiltv <wvera@correo.unimet.edu.ve>
2021-11-29 00:57:57 -0400

    css pestana denegado y waiting

--- src/Pages/Status/Denegate.jsx
+++ src/Pages/Status/Denegate.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import styles from "./Denegate.module.css";
+import { UserContext } from "../../Context/UserContext";
+import { useContext } from "react";
 
 function Denegate() {
-    console.log("a")
+    const { user, setUser } = useContext(UserContext);
     return (
-        <div id={styles.body}>
-            <h1 id={styles.title}>Lamentablemente, su solicitud para registrar en la plataforma no fue aceptada</h1> 
-        </div>
+        <>
+            {!!user ? (
+                <div id={styles.body}>
+                    <h1 id={styles.title}>Lamentablemente, su solicitud para registrar en la plataforma no fue aceptada</h1>
+                </div>
+            ) : (
+                <h1 id={styles.isLoading}>
+                    Cargando...
+                </h1>
+            )}
+        </>
     );
-  }
-  
+}
+
 export default Denegate;
--- src/Pages/Status/Waiting.jsx
+++ src/Pages/Status/Waiting.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "./Waiting.module.css";
-
+import { UserContext } from "../../Context/UserContext";
+import { useContext } from "react";
 function Waiting() {
+    const { user, setUser } = useContext(UserContext);
     return (
-        <div id={styles.body}>
-            <h1 id={styles.title}>Su solicitud para registrase en la plataforma esta siendo revisada</h1>  
-        </div>
+        <>
+            {!!user ? (
+                <div id={styles.body}>
+                    <h1 id={styles.title}>Su solicitud para registrase en la plataforma esta siendo revisada</h1>
+                </div>
+            ) : (
+                <h1 id={styles.isLoading}>
+                    Cargando...
+                </h1>
+            )}
+        </>
     );
-  }
-  
+}
+
 export default Waiting;
