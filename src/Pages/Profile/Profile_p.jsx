69d28f354ce399d8a056d7cbd354dc4847cfdb43
wiltv <wvera@correo.unimet.edu.ve>
2021-11-29 21:06:34 -0400

    Usuario css

--- src/Pages/Profile/Profile_p.jsx
+++ src/Pages/Profile/Profile_p.jsx
@@ -21,16 +21,18 @@ function Profile_p() {
           {(user?.role === "pacient") ? (
             <div id={styles.body}>
               <div class={styles.box1}>
-                <img
-                  id={styles.img1}
-                  src="https://concepto.de/wp-content/uploads/2018/08/persona-e1533759204552.jpg"
-                  alt=""
-                />
                 <div class={styles.info}>
                   <p id={styles.nombre}>
                     Nombre: {user.name} {user.lastName}
                   </p>
                   <p id={styles.rol}>Rol: {user.role}</p>
+                </div>
+                <div id={styles.boximg}>
+                  <img
+                    id={styles.img1}
+                    src="https://concepto.de/wp-content/uploads/2018/08/persona-e1533759204552.jpg"
+                    alt=""
+                  />
                 </div>
               </div>
               <div class={styles.box4}>
@@ -47,10 +49,6 @@ function Profile_p() {
                   </Link>
                 </div>
                 <div class={styles.box3}>
-                  <h2 id={styles.titulo1}>Modificación de Datos</h2>
-                  <p>Ingrese el nuevo nombre: </p>
-                  <p>{user.name}</p>
-
                   <Link to="/Modify_pa">Modificar datos</Link>
                 </div>
               </div>
