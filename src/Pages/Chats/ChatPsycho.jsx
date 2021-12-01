f307656c8508fae624a3a08d2981618fa42128f0
Angélica Tezara Oberto <61010107+angelicaoberto@users.noreply.github.com>
2021-12-01 17:15:21 -0400

    Arreglo CSS chat psicologo

--- src/Pages/Chats/ChatPsycho.jsx
+++ src/Pages/Chats/ChatPsycho.jsx
@@ -58,7 +58,7 @@ export default function ChatPacient(){
     return (
         <>
             <div class={styles.body}>
-            <h1 class = {styles.titulo}>Bienvenido/a al chat con su paciente</h1>
+            <h1 class = {styles.titulo}>Bienvenido/a al chat con tu paciente</h1>
                 <div class = {styles.messages}>
                     {messages.map(message => (
                         <div class={styles.mini}>
