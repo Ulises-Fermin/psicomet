def37788873d2c385dc80b5a621dc76da5674685
Angélica Tezara Oberto <61010107+angelicaoberto@users.noreply.github.com>
2021-12-01 13:09:07 -0400

    Detalles CSS chat psicologo

--- src/Pages/Chats/ChatPsycho.jsx
+++ src/Pages/Chats/ChatPsycho.jsx
@@ -63,7 +63,7 @@ export default function ChatPacient(){
     return (
         <>
             <div class={styles.body}>
-            <h1 class = {styles.titulo}>Bienvenido al chat con tu Paciente</h1>
+            <h1 class = {styles.titulo}>Bienvenido/a al chat con su paciente</h1>
                 <div class = {styles.messages}>
                     {messages.map(message => (
                         <div class={styles.mini}>
@@ -91,7 +91,7 @@ export default function ChatPacient(){
                         onChange={handleOnChange} 
                         placeholder="Escribe aqui tu mensaje..."/>
                         <button type="submit" disabled={!newMessage} id = {styles.button}>
-                        <svg xmlns="http://www.w3.org/2000/svg"  fill="black" class="bi bi-send" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg"  fill="#ffff" class="bi bi-send" viewBox="0 0 16 16">
                         <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
                         </svg>
                         </button>
