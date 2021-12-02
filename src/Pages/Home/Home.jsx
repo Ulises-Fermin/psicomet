ba60b7d366a707955ac5c9d9af2bbe7bbd61dbcf
catatoc <ccarrasquero@correo.unimet.edu.ve>
2021-12-01 21:05:22 -0400

    Links en home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Slideshow from "../../Components/SlideShow/SlideShow"
 import HeroSection from "../../Components/HeroSection";
 
+
 function Home() {
   return (
     <div id={styles.Body}> 
@@ -21,20 +22,29 @@ function Home() {
 
       <div id={styles.Rates}>
         <h1 class={styles.h1AboutUs}>Nuestras Tarifas</h1>
-        <p class={styles.pAboutUs}>En PsicoMet, las tarifas varían de acuerdo a cada especialista, su</p>
-        <p class={styles.pAboutUs}>trayectoria y disponibilidad. Puedes encontrar cada sesión a partir de:</p>
-        <div class={styles.BottomRates}>
-          <h4 class={styles.h4Bottom}>$30 USD o Bs al cambio.</h4>
-          <p class={styles.pBottom}>60 minutos en llamada.</p>
+        <p class={styles.pAboutUs}>En PsicoMet, las tarifas tenemos 5 precios</p>
+        <p class={styles.pAboutUs}>uno para cada tipo de trabajo y especialidad.</p>
+        <div class={styles.BottomRates} >
+          <Link to="/Price"></Link>
+          <h4 class={styles.h4Bottom}>
+            <Link to="/Price">$25, $30, $35, $45 o $50</Link>
+            
+          </h4>
+          <p class={styles.pBottom}>
+            <Link to="/Price">50 minutos en llamada</Link>
+          </p>
         </div>
       </div>
 
       <div id={styles.FAQs}>
-        <h1 class={styles.h1AboutUs}>FAQs</h1>
-        <p class={styles.pAboutUs}>Visita nuestra sección de preguntas frecuentes</p>
-        <p class={styles.pAboutUs}>para informarte acerca nuestros servicios.</p>
+        <h1 class={styles.h1AboutUs}>Testimonios</h1>
+        <p class={styles.pAboutUs}>Visita nuestra sección de testimonios</p>
+        <p class={styles.pAboutUs}>para conocer las opiniones de nuestros clientes.</p>
         <div class={styles.BottomFAQs}>
-          <p class={styles.pFAQs}>Visitar página</p>
+          <p class={styles.pFAQs}>
+            <Link to="/Testimonials">Visitar página</Link>
+          
+          </p>
         </div>
       </div>
     </div>
